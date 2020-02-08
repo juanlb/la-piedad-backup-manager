137246512332ce15c19f0622834fe11427982b12
Juanlb <juanlb@gmail.com>
2020-02-08 01:17:42 -0300

    Use API_URL from en var

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import jsonServerProvider from 'ra-data-json-server';
 import authProvider from './authProvider';
 import customRoutes from './customRoutes';
 
+const API_URL = process.env.REACT_APP_API_URL
 
-
-const dataProvider = jsonServerProvider('http://192.168.0.185:3001');
+const dataProvider = jsonServerProvider(API_URL);
+// const dataProvider = jsonServerProvider('http://192.168.0.185:3001');
 // const dataProvider = jsonServerProvider('https://58bnpw06gh.execute-api.us-east-1.amazonaws.com/Prod');
 const App = () => (
   <Admin  customRoutes={customRoutes} dataProvider={dataProvider} authProvider={authProvider}>
