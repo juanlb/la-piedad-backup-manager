ff0d44384a874f0f2934bac397da4e4e34df5c85
JuanLB <juan.la.battaglia@bytelion.com>
2020-02-21 17:04:41 -0300

    Adding responsiveness to backup list

--- src/backups.js
+++ src/backups.js
@@ -1,17 +1,33 @@
 // in src/users.js
 import React from 'react';
-import { List, Datagrid, TextField, BooleanField, Show, SimpleShowLayout } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
+import {   List, Datagrid, SimpleList, TextField, BooleanField, Show, SimpleShowLayout } from 'react-admin';
+import UserIcon from '@material-ui/icons/Group';
 
-export const BackupList = props => (
-    <List {...props} sort={{ field: 'id', order: 'DESC' }}>
-        <Datagrid rowClick="show">
-            <TextField source="id" />
-            <TextField source="path" />
-            <TextField source="size" />
-            <BooleanField source="on_muleto" />
-        </Datagrid>
-    </List>
-);
+
+export const BackupList = props => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+
+    return (
+        <List {...props} sort={{ field: 'id', order: 'DESC' }} bulkActionButtons={false} >
+            {isSmall ? (
+                <SimpleList
+                 linkType="show"
+                    primaryText={record => record.id}
+                    secondaryText={record => record.size }
+                    tertiaryText={record => record.on_muleto ? 'On Muleto' : 'NO' }
+                />
+            ) : (
+                    <Datagrid rowClick="show">
+                        <TextField source="id" />
+                        <TextField source="path" />
+                        <TextField source="size" />
+                        <BooleanField source="on_muleto" />
+                    </Datagrid>
+                )}
+        </List>
+    );
+}
 
 export const BackupShow = props => (
     <Show {...props}>
